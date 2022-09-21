7cba5e0846a3bfe26f48c41812c31735114e75ca
HugoLeonard <98533637+HugoLeonard@users.noreply.github.com>
2022-09-21 09:38:25 +0200

    Upgrade of the interface
    + Title
    + Put in 2 columns to have less long window

--- Interface_nocity.py
+++ Interface_nocity.py
@@ -9,10 +9,16 @@ def city_choice(cities):
     def quit():
         city2.append(city.get())
         root.destroy()
-        
-    for i in range(0,10):
-        Radiobutton(root, text=cities[i], variable=city, value=cities[i]).grid(row=i, column= 0, sticky=W)
     
-    Button(root, text='Confirmer', command=quit).grid(row=10, column=0)
+    Label(root, text="Faites un choix parmis les 10 meilleures villes pour le mois choisi :").grid(row=0, columnspan=2)
+    
+    for i in range(0,5):
+        Radiobutton(root, text=cities[i], variable=city, value=cities[i]).grid(row=i+1, column= 0, sticky=W)
+    
+    for i in range(5,10):
+        Radiobutton(root, text=cities[i], variable=city, value=cities[i]).grid(row=i-4, column= 1, sticky=W)
+
+    
+    Button(root, text='Confirmer', command=quit).grid(row=10, column=1)
     mainloop()    
     return city2
--- Interface_nomonth.py
+++ Interface_nomonth.py
@@ -10,11 +10,17 @@ def month_choice(dict_meteo):
         month2.append(month.get())
         root.destroy()
         
-    for i in range(0,12):
-        Label(root, text=list(dict_meteo)[i]).grid(row=i, column=0, sticky=E)
-        Radiobutton(root, text=dict_meteo.get(list(dict_meteo)[i]), variable=month, value=list(dict_meteo)[i]).grid(row=i, column= 1, sticky=W)
+    Label(root, text="Faites le choix du mois auquel vous voulez partir :").grid(row=0, columnspan=4)
     
-    Button(root, text='Confirmer', command=quit).grid(row=12, column=1)
+    for i in range(0,6):
+        Label(root, text=list(dict_meteo)[i]).grid(row=i+1, column=0, sticky=E)
+        Radiobutton(root, text=dict_meteo.get(list(dict_meteo)[i]), variable=month, value=list(dict_meteo)[i]).grid(row=i+1, column= 1, sticky=W)
+    
+    for i in range(6,12):
+        Label(root, text=list(dict_meteo)[i]).grid(row=i-5, column=2, sticky=E)
+        Radiobutton(root, text=dict_meteo.get(list(dict_meteo)[i]), variable=month, value=list(dict_meteo)[i]).grid(row=i-5, column= 3, sticky=W)
+
+    Button(root, text='Confirmer', command=quit).grid(row=12, column=3)
     mainloop()
     return month2
 
