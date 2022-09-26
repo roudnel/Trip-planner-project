f7c72b678a3ec8169fdfaa4f1f3cffcee72114e1
sanlaeti <98753736+sanlaeti@users.noreply.github.com>
2022-09-26 18:05:56 +0200

    Update AllCode.py

--- AllCode.py
+++ AllCode.py
@@ -199,7 +199,22 @@ elif month == '':
     print(city)
     print(month2)
     print(best_place)
+    
+best_place2 = best_places(city)
 
-df = pd.DataFrame({'Nom de la ville' :city,'Le mois' : month, 'Lieux a visiter': best_place2})
-print(df)
-df.to_csv('trip_planner',index=False)
+data_dict={'Ville' :city,
+           'Mois' : month, 
+           'Lieux à visiter': best_place2}
+    # Create a Pandas dataframe from the data.
+df = pd.DataFrame.from_dict(data_dict)
+#print(df)
+#result = df.columns
+#print(result)
+
+
+df.to_csv('C:/Users/Laetitia/OneDrive/Documents/MS dS2E/Projet Kevin/Trip-planner-project-main/trip_ planner.csv',index=False)
+
+
+df2 = pd.read_csv('C:/Users/Laetitia/OneDrive/Documents/MS dS2E/Projet Kevin/Trip-planner-project-main/trip_ planner.csv')
+print(df2)
+
