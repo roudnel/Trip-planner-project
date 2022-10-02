7b751bfbf656cc53e9ec89f0a16500c37be3daec
sanlaeti <98753736+sanlaeti@users.noreply.github.com>
2022-10-02 17:53:12 +0200

    Update AllCode.py

--- AllCode.py
+++ AllCode.py
@@ -177,17 +177,26 @@ def best_places(city):
     return tds         ##### PROBLEME CERTAINES VILLES NE MARCHE PAS
 #%% Code final
 
-def search_restaurant(city):
+
     
+def search_restaurant(city):
+
+    #search= input('Search movies')
+    #search = "Paris"
     website = 'https://www.tripadvisor.fr/Restaurants'
     import pyautogui
     path = '/Users/Laetitia/OneDrive/Documents/MS dS2E/Projet Kevin/chromedriver'
     driver = webdriver.Chrome(path)
     driver.get(website)
     time.sleep(5)
+    restaurants=[]
+    time.sleep(5)
+    #pyautogui.press("enter")
+   # drive.find_element("id","onetrust-accept-btn-handler").click()
     driver.find_element("id","onetrust-reject-all-handler").click()
     
     time.sleep(5)
+    #driver.find_element( "xpath",'//*[@id="accept-recommended-btn-handler"]').click()
     driver.find_element("xpath",'//*[@id="component_6"]/div/div/form/input[1]').click()
     driver.find_element("xpath",'//*[@id="component_6"]/div/div/form/input[1]').send_keys(city)
  
@@ -196,10 +205,15 @@ def search_restaurant(city):
     driver.find_element("xpath",'//*[@id="search-filters"]/ul/li[4]/a').click()
     time.sleep(4)
     results=driver.find_elements("xpath",'//div[@class="result-title"]/span')
-    [print(i.text) for i in results]
+    #[print(i.text) for i in results]
+    #le code que j'ai ajouté
+    for j in range (0,10):
+        restaurants.append(results[j].text)
+    #les 10 restaurants se trouvent dans restaurants
+    return restaurants
 
-    restaurants = driver.find_elements("xpath",'//div[@class="result-title"]')
-    driver.close()
+   
+driver.close()
 
 result = Interface_Saisie()
 city = str(result[0])
