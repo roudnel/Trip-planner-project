4bc7a7f592076b3ec0ded5ba4a684f8eefc75b1e
HugoLeonard <98533637+HugoLeonard@users.noreply.github.com>
2022-10-10 18:36:45 +0200

    Add files via upload

--- AllCodeCompact.py
+++ AllCodeCompact.py
@@ -1,4 +1,3 @@
-
 from Interface import Interface_Saisie
 from BestMonth import insert_city, extract
 from BestCities import insert_month
@@ -6,6 +5,7 @@ from Interface_nocity import city_choice
 from Interface_nomonth import month_choice
 from Place2visit import best_places
 from Restaurants import search_restaurant
+from Temp import extract_temp
 import pandas as pd
 
 
@@ -19,35 +19,21 @@ if city == '' and month!='':
     city = city_choice(cities)[0]
     best_place = best_places(city)
     resto=search_restaurant(city)
-    
-    print(city)
-    print(month)
-    print(best_place)
-    print(search_restaurant)
+    temp = extract_temp(city)[month]
  
-
 elif month == '' and city!='':
     bs = insert_city(city)
     dict_meteo = extract(bs)
-    month2 = month_choice(dict_meteo)
+    month = month_choice(dict_meteo)
     best_place = best_places(city)
     resto=search_restaurant(city)
-    print(city)
-    print(month2)
-    print(best_place)
-    print(search_restaurant)
+    temp = extract_temp(city)[month[0]]
     
 else :
     best_place = best_places(city)
     resto=search_restaurant(city)
-    print(city)
-    print(month)
-    print(best_place)
-    print(search_restaurant)
-    
+    temp = extract_temp(city)[month]
      
-
-
 data_dict={ 
            'Lieux à visiter': best_place,
            'Restaurant':resto
@@ -56,10 +42,9 @@ data_dict={
 df = pd.DataFrame.from_dict(data_dict)
 
 
-df.to_csv('C:/Users/Laetitia/OneDrive/Documents/MS dS2E/Projet Kevin/Trip-planner-project-main/trip_ planner.csv',index=False)
-df2 = pd.read_csv('C:/Users/Laetitia/OneDrive/Documents/MS dS2E/Projet Kevin/Trip-planner-project-main/trip_ planner.csv')
-print(df2)
+df.to_csv('/Users/hugol/Advanced Programming/T2/trip_ planner.csv',index=False)
+df2 = pd.read_csv('/Users/hugol/Advanced Programming/T2/trip_ planner.csv')
 
-
-
-
+recap = "A "+ city +" en "+ month +" il fait environ "+ temp
+with open(r'liste.txt', 'w') as fp:
+    fp.write(recap)
