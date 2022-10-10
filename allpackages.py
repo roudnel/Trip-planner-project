ae04b7d75805dd2ae312c323d3eb15f98aa015c6
Roudnel Colin <98753233+roudnel@users.noreply.github.com>
2022-10-11 01:49:24 +0200

    Add files via upload

---
+++ allpackages.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 11 01:39:24 2022
+
+@author: colin
+"""
+
+
+
+pip install streamlit
+pip install BeautifulSoup
+pip install Selenium
+pip install time 
+pip install pandas 
+pip install tkinter
+pip install pyautogui 
