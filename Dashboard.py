1193c05438a4c1aa83c88c48684706a4a34bde16
HugoLeonard <98533637+HugoLeonard@users.noreply.github.com>
2022-10-10 18:35:12 +0200

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -1,23 +1,18 @@
-
 import streamlit as st
 import pandas as pd
 
-
 st.title("Trip Planner")
 st.header(" Préparer vos valises, vous êtes prêt à partir!")
-
-
-st.write(" Voici votre choix")
-
-
-data =pd.read_csv('C:/Users/Laetitia/OneDrive/Documents/MS dS2E/Projet Kevin/Trip-planner-project-main/trip_ planner.csv')
+    
+st.write(" Voici votre choix :")
+    
+liste = open(r'liste.txt', 'r')
+lr = liste.read()
+st.write(lr)
+ 
+data =pd.read_csv('/Users/hugol/Advanced Programming/T2/trip_ planner.csv')
 st.dataframe(data)
-
-
-st.write("Hugo Leonard"+" , " +"Laetitia Santos Moreira"+" ,"+"Roudnel Colin")
-
-
-st.write("Data source: [Ou et quand partir]  https://www.ou-et-quand.net/partir/quand/")
-st.write("https://www.tripadvisor.fr/" )
-st.write("https://partir.ouest-france.fr/meteo/oupartiren")
-                
+    
+st.write("Hugo Leonard, ", "Laetitia Santos Moreira, ", "Roudnel Colin")
+st.write("Data source: ")
+st.write("https://www.ou-et-quand.net/ , https://www.tripadvisor.fr/ , https://partir.ouest-france.fr")
