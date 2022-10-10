074477943d2cc421d86e634629bc8e087015d5d5
sanlaeti <98753736+sanlaeti@users.noreply.github.com>
2022-10-10 14:22:53 +0200

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -1,16 +1,23 @@
+
 import streamlit as st
 import pandas as pd
 
 
 st.title("Trip Planner")
-st.header(" Lieux à visiter, le mois pour partir, et ou partir ")
+st.header(" Préparer vos valises, vous êtes prêt à partir!")
+
+
 st.write(" Voici votre choix")
 
 
-st.write("Data source: [Ou et quand partir] https://www.ou-et-quand.net/partir/quand/"+ "  "+ "     "+  "https://generationvoyage.fr" +" " +"https://partir.ouest-france.fr/meteo/oupartiren")
-    
 data =pd.read_csv('C:/Users/Laetitia/OneDrive/Documents/MS dS2E/Projet Kevin/Trip-planner-project-main/trip_ planner.csv')
 st.dataframe(data)
 
 
-st.write("Hugo Leonard", "Laetitia Santos Moreira", "Roudnel Colin")
+st.write("Hugo Leonard"+" , " +"Laetitia Santos Moreira"+" ,"+"Roudnel Colin")
+
+
+st.write("Data source: [Ou et quand partir]  https://www.ou-et-quand.net/partir/quand/")
+st.write("https://www.tripadvisor.fr/" )
+st.write("https://partir.ouest-france.fr/meteo/oupartiren")
+                
