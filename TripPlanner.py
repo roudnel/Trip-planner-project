1a037e1f970725e62cf4238faa573da83d217bfa
HugoLeonard <98533637+HugoLeonard@users.noreply.github.com>
2022-10-11 10:38:27 +0200

    Adding os.getcwd

--- TripPlanner.py
+++ TripPlanner.py
@@ -1,4 +1,3 @@
-from Interface import Interface_Saisie
 from BestMonth import insert_city, extract
 from BestCities import insert_month
 from Interface_nocity import city_choice
@@ -6,15 +5,23 @@ from Interface_nomonth import month_choice
 from Place2visit import best_places
 from Restaurants import search_restaurant
 from Temp import extract_temp
+from Error import error
 import pandas as pd
+import os
 
 
 result = Interface_Saisie()
 city = str(result[0])
 month = str(result[1])
 
+if city == '' and month == '':
+    while city == '' and month == '':
+        error()
+        result = Interface_Saisie()
+        city = str(result[0])
+        month = str(result[1])
 
-if city == '' and month!='':
+elif city == '' and month!='':
     cities = insert_month(month)
     city = city_choice(cities)[0]
     best_place = best_places(city)
@@ -38,13 +45,12 @@ data_dict={
            'Lieux à visiter': best_place,
            'Restaurant':resto
             }
-    # Create a Pandas dataframe from the data.
+# Create a Pandas dataframe from the data.
 df = pd.DataFrame.from_dict(data_dict)
 
-
-df.to_csv('/Users/hugol/Advanced Programming/T2/trip_ planner.csv',index=False)
-df2 = pd.read_csv('/Users/hugol/Advanced Programming/T2/trip_ planner.csv')
+df.to_csv(os.getcwd() + '/trip_ planner.csv',index=False)
+df2 = pd.read_csv(os.getcwd() + '/trip_ planner.csv')
 
 recap = "A "+ city +" en "+ month +" il fait environ "+ temp
-with open(r'liste.txt', 'w') as fp:
+with open(r'liste.txt', 'w+') as fp:
     fp.write(recap)
